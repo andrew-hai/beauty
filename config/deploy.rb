1d693bf0354aaef2cb2c7705b7e7577f92329d5c
Andrey Melchenko <andrey.melchenko@gmail.com>
2017-05-11 19:16:53 +0300

    Fix asset host path

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.6.0'
+lock '3.8.1'
 
 set :application, 'beauty'
 set :repo_url, 'git@github.com:andrew-hai/beauty.git'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.action_controller.asset_host = 'http://videolist.com.ua'
+  config.action_controller.asset_host = 'http://46.254.19.175'
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
