38c2a3244c0d61b7ec41e36c2daa61fbfb9e189e
Andrey Melchenko <andrey.melchenko@gmail.com>
2017-07-25 14:00:15 +0300

    Switch to Moscow time zone

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,10 @@ module Beauty
     # -- all .rb files in that directory are automatically loaded.
 
     config.i18n.default_locale = :ru
-
     config.i18n.available_locales = [:en, :ru]
+
+    config.time_zone = 'Moscow'
+    config.active_record.default_timezone = :utc
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
